15f66b300375263df7a7eb3ebdb679a953f1991a
Oliwier Perzyński <olek@oliverperzyk.com>
2026-09-02 17:29:22 +0200

    feat(docs): configured Next.js and PostCSS

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,8 +1,40 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
+import { join } from "path"
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
+/**
+ * @summary Configuration of Next.js.
+ * @description This file is used to configure documentation's website configuration of a framework.
+ * @see {@link https://nextjs.org/docs/app/api-reference/config/next-config-js}
+ */
+export default {
+    typedRoutes: true,
+    reactCompiler: true,
+    reactStrictMode: true,
+    turbopack: {
+        root: join(import.meta.dirname, "../.."),
+    },
+    async redirects() {
+        return [
+            {
+                source: "/repository",
+                destination: "https://github.com/oliverperzyk/bedrock-discord",
+                permanent: true,
+            },
+            {
+                source: "/issues",
+                destination: "https://github.com/oliverperzyk/bedrock-discord/issues",
+                permanent: true,
+            },
+            {
+                source: "/github",
+                destination: "https://github.com/oliverperzyk/bedrock-discord",
+                permanent: true,
+            },
+            {
+                source: "/funding",
+                destination: "https://oliverperzyk.com/funding",
+                permanent: true
+            }
+        ]
+    },
+} satisfies NextConfig
---
+++ apps/docs/postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * @summary Configuration of PostCSS.
+ * @description This file is used to configure the PostCSS plugins used in the project, which is used by Tailwind CSS later.
+ * @see {@link https://postcss.org/}
+ */
+export default {
+    plugins: {
+        "@tailwindcss/postcss": {},
+    },
+}
