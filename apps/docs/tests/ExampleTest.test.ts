9111c677224429fb9c3e14d10a384535deaea624
Oliwier Perzyński <olek@oliverperzyk.com>
2026-09-05 20:00:55 +0200

    chore(docs): added tests to documentation website

---
+++ apps/docs/tests/ExampleTest.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "bun:test"
+
+/**
+ * @summary Example test.
+ * @description This test is used to test the example functionality.
+ * @remarks Delete this test after you have added your own tests.
+ */
+test("Example test.", async (): Promise<void> => {
+    expect(2 + 2).toBe(4)
+})
