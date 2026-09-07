0ef8347667e7c18082482dde7baaa542d212538f
Oliwier Perzyński <olek@oliverperzyk.com>
2026-09-07 12:02:38 +0200

    feat(middlewares): added a middleware to verify `User-Agent` header

---
+++ apps/api/src/api/middlewares/headers/UserAgentHeaderMiddleware.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Request, Response } from "express"
+import { BaseMiddleware } from "../base/BaseMiddleware"
+
+/**
+ * @summary Middleware to check the User-Agent header.
+ * @description Middleware makes a basic check whenever a request is made using library's standard User-Agent header.
+ */
+class UserAgentHeaderMiddleware extends BaseMiddleware {
+    /**
+     * @summary Protected constructor.
+     * @description Prevents instantiation of the class.
+     */
+    protected constructor() {
+        super()
+    }
+
+    /**
+     * @summary Handles the middleware request and response.
+     * @description Checks if the User-Agent header is "Bedrock-Discord". If not, sends a 403 status code.
+     * @param request - The request object from Express.js library.
+     * @param response - The response object from Express.js library.
+     * @param next - The next function that moves the request to the next middleware.
+     */
+    public handle(request: Request, response: Response, next: NextFunction): void {
+        const userAgent: string | undefined = request.headers["user-agent"]
+        if (userAgent !== "Bedrock-Discord") {
+            response.sendStatus(403)
+        }
+
+        next()
+    }
+}
+
+export { UserAgentHeaderMiddleware }
